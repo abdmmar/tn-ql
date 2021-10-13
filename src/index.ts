8721b46036bb8ecec25057bee43dad6df3b12c1e
abdmmar <opowae909@gmail.com>
2021-10-13 21:37:11 +0700

    feat: add graphql-playground

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
@@ -6,7 +7,11 @@ import resolvers from './resolvers'
 const PORT = process.env.PORT || 4000
 
 async function startServer() {
-  const server = new ApolloServer({ typeDefs, resolvers })
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  })
 
   const { url } = await server.listen(PORT)
   console.log(`🚀 Server is running on ${url}`)
