77a8d5beb2add38d92a444fd303ee53ac022da0b
abdmmar <opowae909@gmail.com>
2021-10-21 17:02:56 +0700

    feat: connect db to server with prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from 'apollo-server'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { PrismaClient } from '@prisma/client'
 
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
@@ -7,10 +8,14 @@ import resolvers from './resolvers'
 const PORT = process.env.PORT || 4000
 
 async function startServer() {
+  const db = new PrismaClient()
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    context: {
+      db,
+    },
   })
 
   const { url } = await server.listen(PORT)
