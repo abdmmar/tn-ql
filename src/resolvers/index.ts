9458fec5ee10c5b34bb7596ce6a4a55ed39004e4
abdmmar <opowae909@gmail.com>
2021-10-21 17:05:03 +0700

    feat: add scalar Date

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,3 +1,19 @@
 import nationalPark from './nationalPark'
 
-export default [nationalPark]
+import { GraphQLScalarType } from 'graphql'
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  parseValue(value) {
+    return new Date(value)
+  },
+  serialize(value) {
+    return value.toISOString()
+  },
+})
+
+const Date = {
+  Date: dateScalar,
+}
+
+export default [nationalPark, Date]
